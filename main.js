63a19e2e915f5b937410482c03d41bd37267b7f9
GabriellRossi <gabriel.rossi159@gmail.com>
2022-08-19 22:51:15 -0300

    Criado função para abrir e fechar menu no mobile e adicionado labels para acessibilidade

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 function onScroll() {
 
-  const scrollNavigation = document.querySelector("#navigation");
+  const scrollNavigation = document.querySelector('#navigation');
 
   if (scrollY > 0) {
-    scrollNavigation.classList.add('scroll')
+    scrollNavigation.classList.add('scroll');
   } else {
-    scrollNavigation.classList.remove('scroll')
+    scrollNavigation.classList.remove('scroll');
   }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded');
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded');
 }
